805cc3011c455dca1b596c7f5b69868008759cd4
wgkgayani <kaveeshagayani21@gmail.com>
2025-11-11 23:57:18 +0530

    Add comments

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -1,12 +1,13 @@
-import express from "express";
+import express from "express"; //importing express module`
 
-const studentRouter = express.Router();
+const studentRouter = express.Router(); //create studentRouter using express router
 
 studentRouter.get("/", (req, res) => {
   Student.find() //to get all student data from mongodb
     .then((data) => {
+      //if data found
       res.json(data);
     });
 });
 
-export default studentRouter;
+export default studentRouter; //exporting studentRouter to use in other files
