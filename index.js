415deec74f62b9a6f1f5bc12becd6da24cd78f98
wgkgayani <kaveeshagayani21@gmail.com>
2025-11-12 23:54:54 +0530

    update studentRouter file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import Student from "./models/student.js"; //importing student model
+
+import studentRouter from "./routes/studentRouter.js";
 
 const app = express(); //create express app
 
@@ -22,81 +23,7 @@ mongoose
 //
 //
 //
-//
-/*app.get("/", (req, res) => {
-  res.json(
-    // to send response for get request
-    {
-     message: "this is get request",
-   }
-  );
-});*/
-
-app.get("/", (req, res) => {
-  Student.find() //to get all student data from mongodb
-    .then((data) => {
-      res.json(data);
-    });
-});
-
-//mongodb+srv://Admin:123@cluster0.vc3paai.mongodb.net/?appName=Cluster0
-
-app.delete("/", (req, res) => {
-  res.json(
-    // to send response for delete request
-    {
-      message: "this is delete request",
-    }
-  );
-});
-
-app.post("/", (req, res) => {
-  console.log(req.body);
-
-  //student schema
-  /*const studentSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    grade: String,
-    email: String,
-  });
-  //create student model
-  // student = collection name
-  // studentSchema = schema name
-  const Student = mongoose.model("students", studentSchema);*/
-  //to connect to student collection
-
-  const student = new Student({
-    name: req.body.name,
-    age: req.body.age,
-    marks: req.body.marks,
-    email: req.body.email,
-  });
-
-  student //to save student data in mongodb
-    .save()
-    .then(() => {
-      res.json({
-        message: "Student data saved successfully",
-      });
-    })
-    .catch((error) => {
-      res.json({
-        message: "Error saving student data",
-        error: error,
-      });
-    });
-});
-
-app.put("/", (req, res) => {
-  res.json(
-    // to send response for put request
-    {
-      message: "this is put request",
-    }
-  );
-});
-
+app.use("/students", studentRouter);
 //
 //
 //
--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -1,4 +1,5 @@
 import express from "express"; //importing express module`
+import Student from "../models/student.js";
 
 const studentRouter = express.Router(); //create studentRouter using express router
 
@@ -7,6 +8,31 @@ studentRouter.get("/", (req, res) => {
     .then((data) => {
       //if data found
       res.json(data);
+    });
+});
+
+studentRouter.post("/", (req, res) => {
+  console.log(req.body);
+
+  const student = new Student({
+    name: req.body.name,
+    age: req.body.age,
+    marks: req.body.marks,
+    email: req.body.email,
+  });
+
+  student //to save student data in mongodb
+    .save()
+    .then(() => {
+      res.json({
+        message: "Student data saved successfully",
+      });
+    })
+    .catch((error) => {
+      res.json({
+        message: "Error saving student data",
+        error: error,
+      });
     });
 });
 
