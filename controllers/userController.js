70d834007dff874966fe98af368683b09cd3cfb4
wgkgayani <kaveeshagayani21@gmail.com>
2025-11-17 12:29:23 +0530

    update comment

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,8 @@
 import User from "../models/user.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcrypt"; //bcrypt use karanne password hash karanna
 
 export function createUser(req, res) {
-  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10); //hash karanna password eka 10 times
 
   const user = new User({
     // methana User thamai model eke anthimt export karpu User
