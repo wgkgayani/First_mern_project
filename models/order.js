af3efa1292bedfa0c6d5354feef3491fd6968f9a
wgkgayani <kaveeshagayani21@gmail.com>
2025-12-14 22:41:55 +0530

    Create order schema of the mongoose in the order.js

---
+++ models/order.js
@@ -0,0 +1,70 @@
+import mongoose, { Types } from "mongoose";
+
+const orderSchema = mongoose.Schema({
+  orderId: {
+    Type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    Type: String,
+    required: true,
+  },
+  name: {
+    Type: String,
+    required: true,
+  },
+  phone: {
+    Type: String,
+    required: true,
+  },
+  address: {
+    Type: String,
+    required: true,
+  },
+  status: {
+    Type: String,
+    required: true,
+    default: "pending",
+  },
+  totle: {
+    Type: Number,
+    required: true,
+  },
+  products: [
+    {
+      productInfo: {
+        productId: {
+          type: String,
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        altNames: [
+          {
+            type: String,
+          },
+        ],
+        destription: {
+          type: String,
+          required: true,
+        },
+        images: [{ type: String }],
+        labelledPrice: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
