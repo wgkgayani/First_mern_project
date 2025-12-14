9b40b648752e44178d3095ba252b71407a9b29c6
wgkgayani <kaveeshagayani21@gmail.com>
2025-12-14 21:48:10 +0530

    Create update Product router by admin

--- controllers/productController.js
+++ controllers/productController.js
@@ -95,3 +95,61 @@ export async function deleteProduct(req, res) {
     });
   }
 }
+
+export async function updateProduct(req, res) {
+  if (!isAdmin(req)) {
+    res.status(403).json({
+      message: "You are not authorized to update a product",
+    });
+    return;
+  }
+
+  const productId = req.params.productId;
+  const updatingData = req.body;
+
+  try {
+    await Product.updateOne({ productId: productId }, updatingData);
+
+    res.json({
+      message: "Product updated successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal server error",
+      error: err,
+    });
+  }
+}
+
+/*export async function getProductById(req, res) {
+  const productId = req.params.productId;
+
+  try {
+    const product = await Product.findOne({ productId: productId });
+
+    if (Product == null) {
+      res.status(404).json({
+        message: "Product not found",
+      });
+      return;
+    }
+
+    if (product.isAvailable) {
+      res.json(product);
+    } else {
+      if (!isAdmin(req)) {
+        res.status(404).json({
+          message: "Product not found",
+        });
+        return;
+      } else {
+        res.json(product);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal server error",
+      error: err,
+    });
+  }
+}*/
